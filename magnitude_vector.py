01577714a1f5c7e8fbb68704cae30b4018a149bf
usaima <usaimarif007@gmail.com>
2017-11-18 14:50:53 +0500

    Add files via upload

--- magnitude_vector.py
+++ magnitude_vector.py
@@ -8,11 +8,13 @@ import math
 
 def magni(v1):
     
-    tmp1 = v1[0]**2 +  v1[1]**2
+    tmp1 = 0
+    for i in range(len(v1)):
+        tmp1 = tmp1 + v1[i]**2
     result = math.sqrt(tmp1)
         
     return result
 
-a = [1,2]
+a = [1,2,3]
 
 print("Magnitude: ",magni(a))
