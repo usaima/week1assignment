c50deb34efc9831f0c06618372f0c20f44b06051
usaima <usaimarif007@gmail.com>
2017-11-21 17:43:36 +0500

    Update magnitude_vector.py

--- magnitude_vector.py
+++ magnitude_vector.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 15 20:47:34 2017
-
 @author: OPS
 """
 import math
@@ -11,10 +10,17 @@ def magni(v1):
     tmp1 = 0
     for i in range(len(v1)):
         tmp1 = tmp1 + v1[i]**2
-    result = math.sqrt(tmp1)
-        
+        result = math.sqrt(tmp1)
     return result
 
-a = [1,2,3]
+def unit(vec,mag):
+    uv = []
+    for i in range(len(vec)):
+        uv.append(vec[i] / mag)
+    return uv
 
-print("Magnitude: ",magni(a))
+a = [1,2,3]
+m = magni(a)
+u = unit(a,m)
+print("Magnitude: ",m)
+print("Unit Vector ",u)
