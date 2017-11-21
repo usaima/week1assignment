335a415b4f8304f7d8e294a825b361e28b0fda71
usaima <usaimarif007@gmail.com>
2017-11-21 16:35:42 +0500

    Add files via upload

---
+++ anglevector.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 12:25:36 2017
+
+@author: OPS
+"""
+
+def dot(v1,v2):
+    result = 0
+    for i in range (len(v1)):
+        result = result + (v1[i] * v2[i])
+        
+    return result
+
+a = [1,-2,3]
+b = [5, 4,1]
+
+dp = dot(a,b)
+print ("dot product of a and b is ",dp)
+if(dp==0):
+    print("a and b are perpendicular")
+elif(dp>0 or dp<0):
+    print ("a and b are not perpendicular")
